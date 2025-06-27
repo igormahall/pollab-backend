0d38523995e7fbfcc1034f72114b8bf9738a178f
igormahall <igormahall@gmail.com>
2025-06-27 03:30:51 -0400

    Adicionado localhost mobile.

--- DjangoEnquete/settings.py
+++ DjangoEnquete/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = 'RENDER' not in os.environ
 
 # Hosts permitidos dinamicamente
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.2.2']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
